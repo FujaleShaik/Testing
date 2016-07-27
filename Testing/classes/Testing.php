64e02028e6beadd29225496d708fb3fb4d97a0f6
fujales <fujales@localhost>
2016-07-27 11:54:18 +0530

    Initial Commit

---
+++ Testing/classes/Testing.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Testing class
+ * Testing class handles all the functionality related to Testing Module
+ */
+class Testing
+{
+
+    /**
+     *  To Print the test value
+     *  @param int $argument1 First parameter
+     *  @return print the value
+     */
+    public function printTest($argument1)
+    {
+        echo $argument1;
+    }
+
+}
+
+?>
